d1eba76a586085da0c9f6a441df0b1a72f0d5bec
Linh Ho <lindathecoolio@gmail.com>
2022-10-15 04:13:54 -0400

    working hamburger menu

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav__links');
-
-hamburger?.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
